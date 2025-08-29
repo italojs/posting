b96d581a6e37dfdfd52f99d048325835090553f1
italo jose <italo.i@live.com>
2025-08-29 18:37:07 -0300

    fix: update SITE_NAME constant to reflect the correct site name

--- app/utils/constants/index.ts
+++ app/utils/constants/index.ts
@@ -1,5 +1,5 @@
 /* eslint-disable import/prefer-default-export */
 // convenient place to have your site name if you may change it later
-export const SITE_NAME = 'Meteor Microblogging Tutorial';
+export const SITE_NAME = 'Content generator';
 
 export const MAX_POST_LENGTH = 280;
